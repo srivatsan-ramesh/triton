23aac16cb397d7024ca31634fa30c12d6bb3cec7
Jokeren <robinho364@gmail.com>
2025-05-04 16:32:12 -0400

    Update

--- bench/triton_bench/distributed.py
+++ bench/triton_bench/distributed.py
@@ -112,8 +112,7 @@ def routing(logits, n_expts_act, expt_indx=None, EP=1):
         expt_indx, topk_indx = torch.sort(expt_indx, stable=True)
         gate_indx = torch.argsort(topk_indx)
         topk_indx[expt_indx == n_expts_tot + 1] = -1
-        n_paddings = torch.sum(expt_indx == n_expts_tot + 1)
-        gate_indx[gate_indx >= n_paddings] = -1
+        gate_indx[gate_indx >= (expt_indx != n_expts_tot + 1).sum()] = -1
         gate_scal = expt_scal[topk_indx]
         hist = torch.histc(expt_indx, bins=n_expts_tot // EP,
                            max=n_expts_tot // EP - 1)  # histogram of tokens over experts
