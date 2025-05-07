cd2f5d6fb46dd630622dbe9b5c8df5f2c498343a
Jokeren <robinho364@gmail.com>
2025-05-07 10:37:09 -0400

    Update

--- bench/tests/test_distributed.py
+++ bench/tests/test_distributed.py
@@ -182,8 +182,8 @@ def distributed_run(rank, world_size, batch, dim1, dim2, n_expts_tot, n_expts_ac
     w1 = torch.randn((n_expts_tot // EP, dim1, dim2 // TP), device=dev)
     w2 = torch.randn((n_expts_tot // EP, dim2 // TP // 2, dim1), device=dev)
     bg = torch.randn((n_expts_tot, ), device=dev)
-    b1 = torch.randn((dim2 // TP, ), device=dev)
-    b2 = torch.randn((dim1, ), device=dev)
+    b1 = torch.randn((n_expts_tot // EP, dim2 // TP), device=dev)
+    b2 = torch.randn((n_expts_tot // EP, dim1), device=dev)
 
     # gather to full replicas
     w1_list = []
@@ -207,7 +207,16 @@ def distributed_run(rank, world_size, batch, dim1, dim2, n_expts_tot, n_expts_ac
         b1_list = [torch.zeros_like(b1) for _ in range(world_size)]
     dist.gather(b1, b1_list, dst=0)
     if rank == 0:
-        b1_full = torch.cat(b1_list, dim=0)
+        rows = [torch.cat(b1_list[e * TP:(e + 1) * TP], dim=1) for e in range(EP)]
+        b1_full = torch.cat(rows, dim=0)
+    
+    b2_list = []
+    if rank == 0:
+        b2_list = [torch.zeros_like(b2) for _ in range(world_size)]
+    dist.gather(b2, b2_list, dst=0)
+    if rank == 0:
+        rows = [torch.cat(b2_list[e * TP:(e + 1) * TP], dim=1) for e in range(EP)]
+        b2_full = torch.cat(rows, dim=0)
 
     # quantization
     swizzle_opt = {"mx4": {"swizzle_mx_scale": True}}
@@ -248,7 +257,7 @@ def distributed_run(rank, world_size, batch, dim1, dim2, n_expts_tot, n_expts_ac
             rdata = gi = si = None
         x = matmul_ogs(x, w1_full, b1_full, rdata, gather_indx=gi, precision_config=pc1_f)
         x = triton_bench.swiglu.swiglu(x, 1.0, pcs)
-        return matmul_ogs(x, w2_full, b2, rdata, scatter_indx=si, precision_config=pc2_f)
+        return matmul_ogs(x, w2_full, b2_full, rdata, scatter_indx=si, precision_config=pc2_f)
 
     # distributed pass
     def distributed(x):
